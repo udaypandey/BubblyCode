d70499de42f9f0a9afd3a1e1573a2726aea7cedc
Uday Pandey <uday.pandey@crvsh.io>
2021-02-01 06:57:21 +0000

    Added first set of files in

---
+++ Python/hello.py
@@ -0,0 +1,10 @@
+a = 1
+b = 2
+add = a + b
+print(add)
+
+a = 4
+b = 9
+add = a + b  + c
+print(add)
+
---
+++ Swift/Bebs.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+3 + 4 + 5
+
+let a = 3
+var b = 4
+var sum = a + b
+print (sum)
+b = 5
+sum = a + b
+
+let c = 5
+
+
