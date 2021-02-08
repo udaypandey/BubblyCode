fb53090cd478a5ee8bf172cbdd9b457bf482c9fc
Anshruta Pandey <anshruta.pandey@gmail.com>
2021-02-08 13:29:39 +0000

    no message

--- Python/RevereNumber.py
+++ Python/RevereNumber.py
@@ -1,3 +1,13 @@
 # Read a number from the keyboard
 # Print reverse number
 # For eg for 1234567, 7654321
+num = int(input("Enter the integer number: "))  
+sum = 0
+
+while num > 0:
+        
+    remain = num % 10
+    sum = sum * 10 + remain
+    num = num // 10
+    
+print(sum)
