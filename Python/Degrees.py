541811fae49710a84085233a5b961170b48166f8
Anshruta Pandey <anshruta.pandey@gmail.com>
2021-02-08 10:18:51 +0000

    celcius

---
+++ Python/Degrees.py
@@ -0,0 +1,15 @@
+while True:
+    celcius = input("Enter a temperature in celcius: ")
+    enteredCelcius = float(celcius)
+    farenheit = (enteredCelcius * 9/5+32)
+    print(farenheit)
+   
+celcius = input("Enter a temperature in celcius: ")
+enteredCelcius = int(celcius)
+
+
+
+
+
+
+
