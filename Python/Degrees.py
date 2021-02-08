3d85faf0dae61de93c234e44765100c3df2a8283
Uday Pandey <uday.pandey@crvsh.io>
2021-02-08 10:22:20 +0000

    Added python code in

--- Python/Degrees.py
+++ Python/Degrees.py
@@ -1,11 +1,9 @@
 while True:
     celcius = input("Enter a temperature in celcius: ")
     enteredCelcius = float(celcius)
-    farenheit = (enteredCelcius * 9/5+32)
+    farenheit = (enteredCelcius * 9/5 + 32.0)
     print(farenheit)
-   
-celcius = input("Enter a temperature in celcius: ")
-enteredCelcius = int(celcius)
+
 
 
 
---
+++ Python/PrintDigits.py
@@ -0,0 +1,3 @@
+# Read a number from the keyboard
+# Print each digits (starting with unit place) on individual lines
+# For eg for 345, print 5 and then 4 and then 3 on 3 lines
