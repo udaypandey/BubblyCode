b47662c550070a530848b37bf68877182456ed6c
Anshruta Pandey <anshruta.pandey@gmail.com>
2021-02-08 12:47:17 +0000

    no message

--- Python/PrintDigits.py
+++ Python/PrintDigits.py
@@ -1,3 +1,14 @@
 # Read a number from the keyboard
 # Print each digits (starting with unit place) on individual lines
 # For eg for 345, print 5 and then 4 and then 3 on 3 lines
+
+number = input("Enter an integer: ")
+enteredNumber = int(number)
+
+numberFlip = enteredNumber % 10
+numberFlip2 = (enteredNumber // 100) % 10
+numberFlip3  = (enteredNumber // 100) 
+
+
+print(numberFlip * 100 + numberFlip3 * 10 + numberFlip2 )
+
