b241ea6a9202d0091109f0127d3db72e9f43bd52
Uday Pandey <uday.pandey@crvsh.io>
2021-02-08 09:13:57 +0000

    Files updated

---
+++ Python/break.py
@@ -0,0 +1,11 @@
+while True: # while 1
+    while True: # while 2
+        while True: # while 3
+            print("anshruta")
+            break
+        print("bubbles")
+        break
+    print("bebs")
+    break
+
+print("Done")
---
+++ Python/name.py
@@ -0,0 +1,26 @@
+while True:
+    name = input("Enter your name: ").lower()
+
+    if name == "uday" :
+        print("You are very mean.")
+    elif name == "bubbles" :
+        print("You are so nice and amazing!")
+    elif name == "mumma" :
+        print("What a fatso!")
+    elif name == "waffles" :
+        print("You are adorable!")
+    else:
+        print("Hello stranger.")
+
+
+
+
+        
+
+
+
+    
+        
+
+
+        
