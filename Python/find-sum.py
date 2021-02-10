443397255bb53084f4e1d0a493100b795d5967c3
Uday Pandey <uday.pandey@crvsh.io>
2021-02-10 16:30:51 +0000

    Files updated

---
+++ Python/find-sum.py
@@ -0,0 +1,3 @@
+# Write a program that asks the user for a number n and prints the sum of the numbers 1 to n
+# for eg for 4, it will add 1, 2, 3, 4
+
---
+++ Python/multiplication-table.py
@@ -0,0 +1,2 @@
+# Write a program that prints a multiplication table for numbers up to 12.
+
